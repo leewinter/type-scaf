06802c287cdb9f98eb67a1dbbe66ec4e9c5a1155
lee.winter@boxspark.co.uk <lee.winter@vennersys.co.uk>
2024-10-28 07:18:55 +0000

    Restructure project src

--- src/scripts/init/copy-templates.js
+++ src/scripts/init/copy-templates.js
@@ -1,17 +1,21 @@
 const fs = require("fs");
 const path = require("path");
-const { resilientCopy } = require("../utils/file");
+const { resilientCopy } = require("../../utils/file-copy");
 
 module.exports = () => {
   // Templates
   const componentTemplateTargetFilePath = path.join(
     process.cwd(),
-    "type-scaf/templates/component.ejs"
+    "type-scaf",
+    "templates",
+    "component.ejs"
   );
 
   const componentTemplateSourceFilePath = path.join(
     __dirname,
     "..",
+    "..",
+    "init-files",
     "templates",
     "react",
     "component.ejs"

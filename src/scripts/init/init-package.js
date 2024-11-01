3ba2df347285e4d94d4ef008f6d725be3aa97cbb
leewinter <lee.winter@vennersys.co.uk>
2024-11-01 07:49:11 +0000

    Add missing deps to init script

--- src/scripts/init/init-package.js
+++ src/scripts/init/init-package.js
@@ -12,6 +12,14 @@ module.exports = () => {
   // console.log(`Running: ${installEjsCommand}`);
   // execSync(installEjsCommand, { stdio: "inherit" });
 
+  const installYupCommand = "npm install yup";
+  console.log(`Running: ${installYupCommand}`);
+  execSync(installYupCommand, { stdio: "inherit" });
+
+  const installMaterialUiCommand = "npm install @mui/material";
+  console.log(`Running: ${installMaterialUiCommand}`);
+  execSync(installMaterialUiCommand, { stdio: "inherit" });
+
   // Update package.json to include a new script
   const packageJsonPath = path.join(process.cwd(), "package.json");
   const packageJson = require(packageJsonPath);

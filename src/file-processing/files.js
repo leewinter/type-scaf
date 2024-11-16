6f70b1723aaa7d075ae12b389b858f76138a2bc5
Lee Winter <lee.winter@boxspark.co.uk>
2024-11-16 14:01:06 +0000

    Generate templates based off settings

--- src/file-processing/files.js
+++ src/file-processing/files.js
@@ -7,46 +7,6 @@ module.exports = {
     "config",
     "settings.json"
   ),
-  listComponentTemplatePath: (templateType) =>
-    path.join(
-      process.cwd(),
-      ".type-scaf",
-      "templates",
-      templateType,
-      "list-component.ejs"
-    ),
-  formComponentTemplatePath: (templateType) =>
-    path.join(
-      process.cwd(),
-      ".type-scaf",
-      "templates",
-      templateType,
-      "form-component.ejs"
-    ),
-  storyFormTemplatePath: (templateType) =>
-    path.join(
-      process.cwd(),
-      ".type-scaf",
-      "templates",
-      templateType,
-      "form-component.stories.ejs"
-    ),
-  hookRestTemplatePath: (templateType) =>
-    path.join(
-      process.cwd(),
-      ".type-scaf",
-      "templates",
-      templateType,
-      "use-hook-rest.ejs"
-    ),
-  hookLocalTemplatePath: (templateType) =>
-    path.join(
-      process.cwd(),
-      ".type-scaf",
-      "templates",
-      templateType,
-      "use-hook-local.ejs"
-    ),
   typesPath: path.join(
     process.cwd(),
     ".type-scaf",
